a1499b101211db00b49a9cc149f65cf9426c782b
hyukjinlee <hyeokjin1992@gmail.com>
2026-01-17 02:34:07 +0900

    print log

--- external_api/server/server_api.py
+++ external_api/server/server_api.py
@@ -27,6 +27,9 @@ def _get_env_path() -> str:
 def _get_cert_path(relative_path: str) -> str:
     return str((_get_root_path() / relative_path).resolve())
 
+logger.info("API_CA_CERT_PATH =", os.getenv("API_CA_CERT_PATH"))
+logger.info("Resolved CA path =", _get_cert_path("certs/dev_ca.crt"))
+
 # .env 파일의 내용을 환경 변수로 로드합니다. (경로 명시)
 load_dotenv(_get_env_path())
 
